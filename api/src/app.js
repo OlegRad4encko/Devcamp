9ac5b78888e9857f49b9e8cda5124fa4645a780d
= <oleg.rad4encko@gmail.com>
2021-12-08 01:44:07 +0200

    ready

--- api/src/app.js
+++ api/src/app.js
@@ -1,12 +1,19 @@
-const express = require('express')
-const app = express()
-
-app.get('/about', function (req, res) {
-    res.send('about')
-})
+const express = require('express');
+const app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hello World')
-})
+    res.send('Hello World');
+});
 
-app.listen(3000)
+app.post('/users', function (req, res) {
+    res.send('Hello World');
+}); // проверил, Postman поставил, скриншот приложу к сообщению
+
+console.log(process.env.APP_PORT);
+
+// nodemon -r dotenv/config src/app.js
+// Напомните мне спросить
+// об ошибке запуска конфигурации
+// связаной с nodemon
+
+app.listen(process.env.APP_PORT);
